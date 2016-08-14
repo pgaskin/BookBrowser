b6f7cb1fb35186305d0e06dc1473a9536e4fc226
Patrick G <geek1011@outlook.com>
2016-08-14 18:19:27 -0400

    Added compatibility check

--- reader.js
+++ reader.js
@@ -136,7 +136,9 @@ doBook = function(url) {
         document.getElementById("booktitle").innerHTML = meta.bookTitle;
         document.getElementById("bookauthor").innerHTML = meta.creator;
         try {
-            getCoverAsDataURL(Book, function(u){document.getElementById("bookcover").src = u;})
+            getCoverAsDataURL(Book, function(u) {
+                document.getElementById("bookcover").src = u;
+            })
         } catch (e) {}
         BookID = [meta.bookTitle, meta.creator, meta.identifier, meta.publisher].join(":");
         var curpostmp = localStorage.getItem(appid + "|" + BookID + "|curPosCfi");
@@ -214,7 +216,54 @@ doHandleFileInput = function(el) {
     doFileFromFileObject(el.files[0]);
 }
 
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    // Test values; Uncomment to check result …
+
+    // IE 10
+    // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+    // IE 11
+    // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+    // Edge 12 (Spartan)
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+    // Edge 13
+    // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
 checkCompatibility = function() {
+    if (detectIE() === false) {} else {
+        return false;
+    }
     if (window.FileReader && window.FileReader.prototype.readAsArrayBuffer) {} else {
         return false;
     }
@@ -347,8 +396,8 @@ document.getElementById("book").innerHTML = "<div class=\"message info\">Please 
 if (checkCompatibility()) {
 
 } else {
-    alert("You are using an incompatible browser. Try using a modern browser such as Google Chrome or Mozilla Firefox.");
-    document.getElementById("book").innerHTML = "<div class=\"message error\">You are using an incompatible browser. Try using a modern browser such as Google Chrome or Mozilla Firefox. If you think this was a mistake, then you can <a href=\"http://github.com/geek1011/ePubViewer/issues\">report an issue</a>.</div>";
+    alert("You are using an incompatible browser. Try using a different browser such as Google Chrome or Mozilla Firefox.");
+    document.getElementById("book").innerHTML = "<div class=\"message error\">You are using an incompatible browser. Try using a different browser such as Google Chrome or Mozilla Firefox. If you think this was a mistake, then you can <a href=\"http://github.com/geek1011/ePubViewer/issues\">report an issue</a>.</div>";
     document.querySelector("nav").style.display = "none";
 }
 document.body.classList.add("not-loaded")
