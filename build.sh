e8a847147eab002d743e3698eada3e595a911618
Patrick G <geek1011@outlook.com>
2017-06-22 17:25:17 -0400

    Remove release steps from build script because it is in the release script

--- build.sh
+++ build.sh
@@ -4,12 +4,6 @@ cd scripts
 
 export GITHUB_TOKEN
 
-# To release a new version, create a new tag first, and push with git push --tags
-# The release publishing requires a github token to be set in the var GITHUB_TOKEN
-# Add any dependencies to install-dependencies.sh
-
 ./clean.sh || exit 1
 ./install-dependencies.sh || exit 1
 ./build-all.sh || exit 1
-./release-notes.sh || exit 1
-./publish-release.sh || exit 1
