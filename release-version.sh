7fdb91d7f132e5e98fc2e148c8fd7a1e38a9d0cb
Patrick G <geek1011@outlook.com>
2017-06-22 16:53:23 -0400

    Added release script

---
+++ release-version.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+set -e
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 v#.#.#"
+    exit 1
+fi
+
+read -r -p "Create a new tag for version $1, push it, and build a release? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        echo "Creating release"
+        ;;
+    *)
+        echo Aborted
+        exit 1
+        ;;
+esac
+
+git tag $1
+git push --tags
+./build.sh
