c9208bf073a6892e4427d1e238eab6ae4069f81c
Patrick G <geek1011@outlook.com>
2017-06-22 17:26:42 -0400

    Added check for github token to release script

--- release-version.sh
+++ release-version.sh
@@ -8,6 +8,11 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "Please set the GITHUB_TOKEN variable to release a new version"
+    exit 1
+fi
+
 read -r -p "Create a new tag for version $1, push it, and build a release? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY]) 
