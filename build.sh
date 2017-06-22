eaba46df42dad00fb094736528ae25bd3642e506
Patrick G <geek1011@outlook.com>
2017-06-22 15:56:29 -0400

    Fixed bug in build script

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ echo "--> Getting version info"
 # Gets current tag, or else previous-tag+commit-hash~dev
 APP_VERSION="$(git name-rev --name-only --tags HEAD | sed "s/undefined/$(git describe --abbrev=0 --tags)+$(git rev-parse --short HEAD)-dev/")"
 echo "APP_VERSION=$APP_VERSION" | indent
-IS_DEV=$(if [[ "$(git name-rev --name-only --tags HEAD)" != "undefine
+IS_DEV=$(if [[ "$(git name-rev --name-only --tags HEAD)" == "undefine
 d" ]];then echo true;else echo false;fi)
 echo "IS_DEV=$IS_DEV" | indent
 
