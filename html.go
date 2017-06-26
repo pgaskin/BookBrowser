78480b2ee2ba13f94a8fe2fd4ebe67bb89308d9f
Patrick G <geek1011@outlook.com>
2017-06-26 16:14:24 -0400

    Limit width of navbar to make it look better

--- html.go
+++ html.go
@@ -22,32 +22,34 @@ func pageHTML(title string, content string) string {
 </head>
 <body>
 <nav class="navbar navbar-default navbar-fixed-top">
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-		<span class="sr-only">Toggle navigation</span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		</button>
-		<a class="navbar-brand" href="/books/">BookBrowser</a>
-	</div>
-	<div id="navbar" class="navbar-collapse collapse">
-		<ul class="nav navbar-nav">
-		<li><a href="/books/">Books</a></li>
-		<li><a href="/authors/">Authors</a></li>
-		<li><a href="/series/">Series</a></li>
-		<li><a href="/random/">Random Book</a></li>
-		</ul>
-		<ul class="nav navbar-nav navbar-right" style="padding-right:20px">
-			<form class="navbar-form" role="search" method="GET" action="/search/">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Search" name="q" id="q">
-				<div class="input-group-btn">
-					<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/books/">BookBrowser</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+			<li><a href="/books/">Books</a></li>
+			<li><a href="/authors/">Authors</a></li>
+			<li><a href="/series/">Series</a></li>
+			<li><a href="/random/">Random Book</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right" style="padding-right:20px">
+				<form class="navbar-form" role="search" method="GET" action="/search/">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Search" name="q" id="q">
+					<div class="input-group-btn">
+						<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+					</div>
 				</div>
-			</div>
-			</form>
-		</ul>
+				</form>
+			</ul>
+		</div>
 	</div>
 </nav>
 <div class="container">
