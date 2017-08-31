0a3d7d1a918f75ca226d9a778ea6b5f634f63722
Patrick G <geek1011@outlook.com>
2017-08-31 15:47:55 -0400

    Fixed windows builds

--- bookbrowser.go
+++ bookbrowser.go
@@ -116,16 +116,7 @@ func main() {
 			log.Fatalln("Fatal error: no books found")
 		}
 
-		sigsa := make(chan os.Signal, 1)
-		signal.Notify(sigsa, syscall.SIGUSR1)
-		go func() {
-			for _ = range sigsa {
-				go func() {
-					log.Println("Booklist refresh triggered by SIGUSR1")
-					server.RefreshBookIndex()
-				}()
-			}
-		}()
+		addRefreshSignalListener(server)
 
 		err = server.Serve()
 		if err != nil {
---
+++ refreshsignal_windows.go
@@ -0,0 +1,4 @@
+package main
+
+func addRefreshSignalListener(server *Server) {
+}
