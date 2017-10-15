198f89cebe33bdbdbd233f715259f0e6c1dbe4a3
Patrick G <geek1011@outlook.com>
2017-10-15 14:40:29 -0400

    Fixed tests

--- formats/formats_test.go
+++ formats/formats_test.go
@@ -1,6 +1,7 @@
 package formats
 
 import (
+	"image"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,7 +15,7 @@ func TestRegisterFormat(t *testing.T) {
 	RegisterFormat(&Format{
 		Glob:      "**/*.test",
 		Extension: "test",
-		Indexer: func(filename string) (*models.Book, *models.Cover, error) {
+		Indexer: func(filename string) (*models.Book, image.Image, error) {
 			return nil, nil, nil
 		},
 	})
