ad1e625fef587c76e92ff60223b026a355e7a7b9
Patrick G <geek1011@outlook.com>
2018-09-27 11:42:50 -0400

    Switched to pflag for flag parsing

--- bookbrowser.go
+++ bookbrowser.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"syscall"
+	"time"
 
 	_ "github.com/geek1011/BookBrowser/formats/epub"
 	_ "github.com/geek1011/BookBrowser/formats/mobi"
@@ -15,7 +18,7 @@ import (
 	"github.com/geek1011/BookBrowser/server"
 	"github.com/geek1011/BookBrowser/util"
 	"github.com/geek1011/BookBrowser/util/sigusr"
-	"github.com/urfave/cli"
+	"github.com/spf13/pflag"
 )
 
 var curversion = "dev"
@@ -31,111 +34,100 @@ func main() {
 		deftempdir = filepath.Join(workdir, "_temp")
 	}
 
-	app := cli.NewApp()
-	app.Name = "BookBrowser"
-	app.Usage = "Web-based eBook server supporting ePub and PDF."
-	app.Version = curversion
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "bookdir, b",
-			Value: workdir,
-			Usage: "Load books from `DIR`. The directory must exist.",
-		},
-		cli.StringFlag{
-			Name:  "tempdir, t",
-			Value: deftempdir,
-			Usage: "Use `DIR` as the location for storing temporary files such as cover thumbnails. The directory is created on start and deleted on exit, unless it already exists.",
-		},
-		cli.StringFlag{
-			Name:  "addr, a",
-			Value: ":8090",
-			Usage: "`ADDR` is the address to bind the server to. It is in the format IP:PORT. The IP is optional.",
-		},
-		cli.BoolFlag{
-			Name:  "nocovers, n",
-			Usage: "Do not index covers",
-		},
+	bookdir := pflag.StringP("bookdir", "b", workdir, "the directory to load books from (must exist)")
+	tempdir := pflag.StringP("tempdir", "t", deftempdir, "the directory to store temp files such as cover thumbnails (created on start, deleted on exit unless already exists)")
+	addr := pflag.StringP("addr", "a", ":8090", "the address to bind the server to ([IP]:PORT)")
+	nocovers := pflag.BoolP("nocovers", "n", false, "do not index covers")
+	help := pflag.BoolP("help", "h", false, "Show this help text")
+	sversion := pflag.Bool("version", false, "Show the version")
+	pflag.Parse()
+
+	if *sversion {
+		fmt.Printf("BookBrowser %s\n", curversion)
+		os.Exit(0)
 	}
-	app.HideHelp = true
-	app.Action = func(c *cli.Context) {
-		bookdir := c.String("bookdir")
-		tempdir := c.String("tempdir")
-		nocovers := c.Bool("nocovers")
-		noRemoveTempDir := false
 
-		addr := c.String("addr")
-
-		log.Printf("BookBrowser %s\n", curversion)
-
-		if _, err := os.Stat(bookdir); err != nil {
-			if os.IsNotExist(err) {
-				log.Fatalf("Fatal error: book directory %s does not exist\n", bookdir)
-			}
+	if *help || pflag.NArg() != 0 {
+		fmt.Fprintf(os.Stderr, "Usage: BookBrowser [OPTIONS]\n\nVersion:\n  BookBrowser %s\n\nOptions:\n", curversion)
+		pflag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\n")
+		if runtime.GOOS == "windows" {
+			time.Sleep(time.Second * 2)
 		}
+		os.Exit(1)
+	}
 
-		if fi, err := os.Stat(tempdir); err == nil || (fi != nil && fi.IsDir()) {
-			noRemoveTempDir = true
-			if tempdir == deftempdir {
-				noRemoveTempDir = false
-			}
-		}
+	noRemoveTempDir := false
 
-		bookdir, err = filepath.Abs(bookdir)
-		if err != nil {
-			log.Fatalf("Fatal error: Could not resolve book directory %s: %s\n", bookdir, err)
-		}
+	log.Printf("BookBrowser %s\n", curversion)
 
-		if _, err := os.Stat(tempdir); os.IsNotExist(err) {
-			os.Mkdir(tempdir, os.ModePerm)
-		}
-
-		tempdir, err = filepath.Abs(tempdir)
-		if err != nil {
-			log.Fatalf("Fatal error: Could not resolve temp directory %s: %s\n", tempdir, err)
-		}
-
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-		go func() {
-			<-sigs
-			if noRemoveTempDir {
-				log.Println("Not removing temp dir because dir already existed at start")
-			} else {
-				log.Println("Cleaning up temp dir")
-				os.RemoveAll(tempdir)
-			}
-			os.Exit(0)
-		}()
-
-		if !strings.Contains(addr, ":") {
-			log.Fatalln("Invalid listening address")
-		}
-
-		sp := strings.SplitN(addr, ":", 2)
-		if sp[0] == "" {
-			ip := util.GetIP()
-			if ip != nil {
-				log.Printf("This server can be accessed at http://%s:%s\n", ip.String(), sp[1])
-			}
-		}
-
-		s := server.NewServer(addr, bookdir, tempdir, curversion, true, nocovers)
-		go func() {
-			s.RefreshBookIndex()
-			if len(s.Indexer.BookList()) == 0 {
-				log.Fatalln("Fatal error: no books found")
-			}
-		}()
-
-		sigusr.Handle(func() {
-			log.Println("Booklist refresh triggered by SIGUSR1")
-			s.RefreshBookIndex()
-		})
-
-		err = s.Serve()
-		if err != nil {
-			log.Fatalf("Error starting server: %s\n", err)
+	if _, err := os.Stat(*bookdir); err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("Error: book directory %s does not exist\n", *bookdir)
 		}
 	}
-	app.Run(os.Args)
+
+	if fi, err := os.Stat(*tempdir); err == nil || (fi != nil && fi.IsDir()) {
+		noRemoveTempDir = true
+		if *tempdir == deftempdir {
+			noRemoveTempDir = false
+		}
+	}
+
+	*bookdir, err = filepath.Abs(*bookdir)
+	if err != nil {
+		log.Fatalf("Error: could not resolve book directory %s: %v\n", *bookdir, err)
+	}
+
+	if _, err := os.Stat(*tempdir); os.IsNotExist(err) {
+		os.Mkdir(*tempdir, os.ModePerm)
+	}
+
+	*tempdir, err = filepath.Abs(*tempdir)
+	if err != nil {
+		log.Fatalf("Error: could not resolve temp directory %s: %v\n", *tempdir, err)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		if noRemoveTempDir {
+			log.Println("Not removing temp dir because dir already existed at start")
+		} else {
+			log.Println("Cleaning up temp dir")
+			os.RemoveAll(*tempdir)
+		}
+		os.Exit(0)
+	}()
+
+	if !strings.Contains(*addr, ":") {
+		log.Fatalln("Error: invalid listening address")
+	}
+
+	sp := strings.SplitN(*addr, ":", 2)
+	if sp[0] == "" {
+		ip := util.GetIP()
+		if ip != nil {
+			log.Printf("This server can be accessed at http://%s:%s\n", ip.String(), sp[1])
+		}
+	}
+
+	s := server.NewServer(*addr, *bookdir, *tempdir, curversion, true, *nocovers)
+	go func() {
+		s.RefreshBookIndex()
+		if len(s.Indexer.BookList()) == 0 {
+			log.Fatalln("Fatal error: no books found")
+		}
+	}()
+
+	sigusr.Handle(func() {
+		log.Println("Booklist refresh triggered by SIGUSR1")
+		s.RefreshBookIndex()
+	})
+
+	err = s.Serve()
+	if err != nil {
+		log.Fatalf("Error starting server: %s\n", err)
+	}
 }
