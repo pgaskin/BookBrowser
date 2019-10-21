c462fc82402017dcacc9ad68e7358decd0dc78df
Patrick Gaskin <patrick@pgaskin.net>
2019-10-21 13:30:39 -0400

    Removed vendor dir

--- vendor/github.com/sblinch/mobi/idxt.go
+++
@@ -1,7 +0,0 @@
-package mobi
-
-type mobiIdxt struct {
-	Identifier [4]byte  `format:"string"`
-	Offset     []uint16 /* mobiIndx.HeaderLenght + len(mobiTagx.HeaderLenght) */
-	//Unk1       uint16   // Pad with zeros to make it multiples of 4?
-}
